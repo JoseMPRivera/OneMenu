f1e4cae2a4ee0af34628e2adaf8bb19fa63b9ff4
Alex Collado <alexaac14@gmail.com>
2020-02-23 17:27:08 -0800

    profile ux

---
+++ app/src/main/java/com/aje/onemenu/FoodPreference.java
@@ -0,0 +1,14 @@
+package com.aje.onemenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodPreference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_preference);
+    }
+}
