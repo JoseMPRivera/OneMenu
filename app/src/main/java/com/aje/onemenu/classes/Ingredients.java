e9178be69f12b5b3fa4b1522deb4188ace0eb739
Alex Collado <alexaac14@gmail.com>
2020-05-05 22:52:21 -0700

    added more ingredients

--- app/src/main/java/com/aje/onemenu/classes/Ingredients.java
+++ app/src/main/java/com/aje/onemenu/classes/Ingredients.java
@@ -39,23 +39,32 @@ public class Ingredients {
         veggie.add("onion");
         veggie.add("potato");
         veggie.add("tomato");
-
         veggie.add("edamame");
         veggie.add("cilantro");
         veggie.add("celery");
-
         veggie.add("cucumber");
         veggie.add("peas");
         veggie.add("pepper");
         veggie.add("mushroom");
         veggie.add("avocado");
-
         veggie.add("basil");
         veggie.add("arugula");
         veggie.add("asparagus");
         veggie.add("carrot");
         veggie.add("tulipe");
         veggie.add("spinach");
+        veggie.add("bell pepper");
+        veggie.add("orange");
+        veggie.add("lentil");
+        veggie.add("parsley");
+        veggie.add("chickpeas");
+        veggie.add("couscous");
+        veggie.add("shallots");
+        veggie.add("zucchini");
+        veggie.add("scallion");
+        veggie.add("olives");
+
+
 
         Collections.sort(veggie);
     }
@@ -80,6 +89,39 @@ public class Ingredients {
         misc.add("tortilla");
         misc.add("rice");
         misc.add("sour cream");
+        misc.add("worcestershire sauce");
+        misc.add("butter");
+        misc.add("pecan");
+        misc.add("chocolate");
+        misc.add("grits");
+        misc.add("walnut");
+        misc.add("peanut");
+        misc.add("truffle");
+        misc.add("cinnamon");
+        misc.add("ginger");
+        misc.add("turmeric");
+        misc.add("coriander");
+        misc.add("cumin");
+        misc.add("cayenne");
+        misc.add("honey");
+        misc.add("paprika");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         Collections.sort(misc);
     }
